d1cfafe3c3feabf30e3776a0fbc2b345e9466df1
Luke Arntson <arntsonl@gmail.com>
2016-03-02 16:40:03 -0500

    F

--- scripts/fuzz.js
+++ scripts/fuzz.js
@@ -1,7 +1,7 @@
 function makeid(len)
 {
     var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\\[]{} ";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\\[]{}!@#$%^&*)()_ ";
     for( var i=0; i < len; i++ )
         text += possible.charAt(Math.floor(Math.random() * possible.length));
 
@@ -15,19 +15,16 @@ var jquery = document.createElement('script');
 jquery.src = 'https://code.jquery.com/jquery-2.2.1.min.js';
 document.head.appendChild(jquery);
 
-for(var i = 0; i < 1; i++)
-{
+setInterval(function(){
 	// lets say we were owned in this channel
 	var data = {
 		"filenames": [],
-		"message": makeid(100),
+		"message": makeid(1000),
 		"channel_id": channel,
 		"pending_post_id": Math.random().toString(36).substring(7),
 		"user_id": "w6tj384ui788jfani5m5i6ws6a",
 		"create_at":1,
 		"state":"loading"
 	};
-	setTimeout(function(){
-		$.post('/api/v1/channels/'+channel+'/create', JSON.stringify(data));
-	}, 1000);s
-}
+	$.post('/api/v1/channels/'+channel+'/create', JSON.stringify(data));
+}, 1000);
