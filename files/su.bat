7c4da0c6073dad5a97a397eff0c541d2f6380380
Luke Arntson <arntsonl@gmail.com>
2017-03-07 14:35:41 -0500

    Flagged...

--- files/su.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Stop-Process -processname *screen* > c:\windows\temp\su.ps1
-echo Start-Process c:\windows\system32\notepad.exe >> c:\windows\temp\su.ps1
-powershell -executionPolicy bypass -command "c:\windows\temp\su.ps1"
-pause
